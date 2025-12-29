a1506f2e64cc3784e9154426c967051388f8afba
Danial Zamri <74848@siswa.unimas.my>
2025-12-29 19:26:54 +0800

    fix: update image dimensions in Divider component for consistency

--- src/parts/Divider.tsx
+++ src/parts/Divider.tsx
@@ -3,9 +3,9 @@ import divider from "../assets/divider.webp";
 const Divider = () => {
     return (
         <div className="flex flex-row">
-            <img className="h-5 w-60 hidden md:block" src={divider} width="240" height="20" alt="divider" />
-            <img className="h-5 w-60 hidden md:block" src={divider} width="240" height="20" alt="divider" />
-            <img className="h-5 w-60 hidden md:block" src={divider} width="240" height="20" alt="divider" />
+            <img className="h-5 w-[186px] hidden md:block" src={divider} width="186" height="20" alt="divider" />
+            <img className="h-5 w-[186px] hidden md:block" src={divider} width="186" height="20" alt="divider" />
+            <img className="h-5 w-[186px] hidden md:block" src={divider} width="186" height="20" alt="divider" />
         </div>    
     )
 }
