1478b798df030a39099ea6246f55f0267aa1f9c6
Danial Zamri <74848@siswa.unimas.my>
2025-12-29 19:44:01 +0800

    fix: replace list items with divs for navigation links for improved semantics

--- src/parts/Navigation.tsx
+++ src/parts/Navigation.tsx
@@ -15,43 +15,43 @@ const Navigation = () => {
         <aside className="mb-auto lg:w-[339px] bg-blue-100 border border-blue-300 rounded-xl shadow-md opacity-90">
             <nav className="space-y-2 mb-4">
               <h2 className="text-blue-600 font-bold text-lg text-center p-4">site navigation</h2>
-              <ul>
+              <div className="">
                 <div className="flex justify-center">
                   <img className="h-4 w-4" src={home} alt="home icon"/>
                   <Link className="hover:animate-wiggle hover:underline" to="/">
-                    <li className={location.pathname === "/" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>home</li>
+                    <div className={location.pathname === "/" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>home</div>
                   </Link>       
                 </div>
                 <div className="flex justify-center"> 
                   <img className="h-4 w-4" src={about} alt="about icon"/>
                   <Link className="hover:animate-wiggle hover:underline" to="/about">
-                    <li className={location.pathname === "/about" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>about</li>
+                    <div className={location.pathname === "/about" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>about</div>
                   </Link>
                 </div>
                 <div className="flex justify-center">
                   <img className="h-4 w-4" src={blog} alt="blog icon"/>
                   <Link className="hover:animate-wiggle hover:underline" to="/blog">
-                    <li className={location.pathname === "/blog" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>blog</li>
+                    <div className={location.pathname === "/blog" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>blog</div>
                   </Link>       
                 </div>
                 <div className="flex justify-center">
                   <img className="h-4 w-4" src={art} alt="art icon"/>
                   <Link className="hover:animate-wiggle hover:underline" to="/art">
-                    <li className={location.pathname === "/art" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>art</li>
+                    <div className={location.pathname === "/art" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>art</div>
                   </Link>             
                 </div>
                 <div className="flex justify-center">
                   <img className="h-4 w-4" src={cats} alt="videos icon"/>
                   <Link className="hover:animate-wiggle hover:underline" to="/videos">
-                    <li className={location.pathname === "/videos" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>videos</li>
+                    <div className={location.pathname === "/videos" ? "text-blue-700 pl-1 text-sm text-center font-bold" : "text-blue-500 pl-1 text-sm text-center font-bold"}>videos</div>
                   </Link>             
                 </div>
-                <li className="text-sm text-center font-bold text-blue-500">another page (maybe)</li>
+                <div className="text-sm text-center font-bold text-blue-500">another page (maybe)</div>
                 <div className="flex justify-center space-x-1">
                     <img className="h-4 w-4" src={cursor} width="16" height="16" alt="cursor icon"/>
                     <ToggleCursor />  
                 </div>
-              </ul>
+              </div>
             </nav>
           </aside>
     )
